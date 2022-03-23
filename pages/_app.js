f2b36a30f76bde63c2d60202896896f1b3007fa7
Not Default Profile <gamingc340@gmail.com>
2022-03-23 14:47:32 -0700

    home close

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 import "../styles/list.css"
+import "../public/logo.png"
+import "../public/DarkLogo.png"
+import "../styles/home.css"
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -14,6 +14,29 @@ export default function Home() {
             <button className="homeAdopt">Adopt Today</button>
         </div>
       </div>
+      <div className="gallery" style={{marginTop: "10hv", height: "60vh", width: "100vw", display: "inline-flex", justifyContent: "center", textAlign: "center", marginTop: "13%"}}>
+        <div className="animal1 animalBanner" style={{backgroundColor: "black", color: "white", width: "25%"}}>
+          <h1 className="name">Henry</h1>
+          <div className="text">
+            <p className="age">{Math.ceil(Math.random() * 7 + 3)} Years -</p> <p className="breed">German Shepard</p>
+          </div>
+        </div>
+        <div className="animal2 animalBanner" style={{backgroundColor: "black", color: "white", width: "25%", marginLeft: "5%"}}>
+          <h1 className="name">Jim</h1>
+          <div className="text">
+            <p className="age">{Math.ceil(Math.random() * 7 + 3)} Years -</p> <p className="breed"> Parrot</p>
+          </div>
+        </div>
+        <div className="animal3 animalBanner" style={{backgroundColor: "black", color: "white", width: "25%", marginLeft: "5%"}}>
+          <h1 className="name">Tom</h1>
+          <div className="text">
+            <p className="age">{Math.ceil(Math.random() * 7 + 3)} Years -</p> <p className="breed"> Bearded Dragon</p>
+          </div>
+        </div>
+      </div>
+      <div className="events" style={{marginTop: "10vh", height: "60vh", width: "100vw", display: "inline-flex", justifyContent: "center", textAlign: "center", marginTop: "13%"}}>
+        //events will go here
+      </div>
       <div style={{position: "relative", bottom: "0"}}>
         <Divider style={{backgroundColor: "gray", color: "gray", height: "5px"}}/>
         <footer className="footer" style={{backgroundColor: "black", color: "white", width: "100vw", height: "23vh"}}></footer>
