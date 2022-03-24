820c6ab11e10527d792eb8aa12b993215faf4440
John McFarland <Jmcfar187@west-mec.local>
2022-03-24 13:45:40 -0700

    Sign Up

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,13 @@ import "semantic-ui-css/semantic.min.css";
 import '../styles/globals.css'
 import "../styles/list.css"
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    
+      <Component {...pageProps} />
+    
+  );
 }
 
 export default MyApp
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,15 +1,49 @@
 import React from 'react'
-import { Form, Message } from 'semantic-ui-react'
+import { Button, Form, Message, Segment } from 'semantic-ui-react'
+import Layout from './components/layout/Layout'
 
 const signup = () => {
   return (
-    <>
+    <Layout>
+    <Segment style = {{justifyContent: 'center'}}>
 
       
-        <Message  content = "Sign up" header="Sign in" icon="meh" color='teal'/>
-      
+        <Message  content = "Sign up" header="Sign in" icon="meh" color='orange'/>
+        <Form.Input
+            required
+            label="Email"
+            placeholder="Email"
+            name="Email"
+            // value={name}
+            // onChange={handleChange}
+            icon="envelope"
+            iconPosition="left"
+          />
+        <Form.Input
+            required
+            label="Name"
+            placeholder="name"
+            name="name"
+            // value={name}
+            // onChange={handleChange}
+            icon="user"
+            iconPosition="left"
+          />
+        <Form.Input
+            required
+            label="Password"
+            placeholder="Password"
+            name="Password"
+            // value={name}
+            // onChange={handleChange}
+            icon="eye"
+            iconPosition="left"
+          />
+          <Button color='orange' content = 'Sign Up!' ></Button>
+          
 
-    </>
+    </Segment>
+    </Layout>
   )
 }
 
