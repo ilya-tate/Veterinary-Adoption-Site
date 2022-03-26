a68e36f487b947b15e189b9d0503dff1de624ed0
Ethan Philpott <Ephilp288@west-mec.local>
2022-03-25 18:31:57 -0700

    fixed errro

--- components/maps/Map.jsx
+++ components/maps/Map.jsx
@@ -1,18 +1,18 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
-import styles from "../../styles/Map.module.scss"
+// import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+// import styles from "../../styles/Map.module.scss"
 
-const Map = () => {
-    return <MapContainer center={[51.505, -0.09]} zoom={13} className={styles.map}> 
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-}
+// const Map = () => {
+//     return <MapContainer center={[51.505, -0.09]} zoom={13} className={styles.map}> 
+//     <TileLayer
+//       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//     />
+//     <Marker position={[51.505, -0.09]}>
+//       <Popup>
+//         A pretty CSS3 popup. <br /> Easily customizable.
+//       </Popup>
+//     </Marker>
+//   </MapContainer>
+// }
 
-export default Map
+// export default Map
