979da86ecb5448438b36b6da5bd8d3dbd2b23f22
John McFarland <Jmcfar187@west-mec.local>
2022-03-28 14:52:17 -0700

    sign up working

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,49 +1,111 @@
+import axios from 'axios';
 import React from 'react'
 import { Button, Form, Message, Segment } from 'semantic-ui-react'
+import { useState } from 'react';
 import Layout from './components/layout/Layout'
+import { setToken } from './util/auth';
+
 
 const signup = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: ""
+  });
+
+
+  const options = [
+    { key: 's', text: 'student', value: 'student' },
+    { key: 't', text: 'teacher', value: 'teacher' },
+
+  ]
+
+
+  const { name, email, password, role } = user;
+  const [media, setMedia] = useState(null);
+  const [mediaPreview, setMediaPreview] = useState(null);
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+
+      const res = await axios.post('/api/v1/user/signup', {
+        user
+      })
+      setToken(res.data)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  const handleChange = (e) => {
+    const { name, value, files, innerText } = e.target;
+
+    if (name === "media" && files.length) {
+      setMedia(() => files[0]);
+      setMediaPreview(() => URL.createObjectURL(files[0]));
+      setHighlighted(true);
+    } else if (innerText) {
+      setUser((prev) => ({ ...prev, role: innerText }));
+    } else {
+      setUser((prev) => ({ ...prev, [name]: value }));
+    }
+  };
+
   return (
     <Layout>
       <Segment style={{ justifyContent: 'center' }}>
 
 
         <Message content="Sign up" header="Sign in" icon="meh" color='orange' />
-        <Form.Input
-          required
-          label="Email"
-          placeholder="Email"
-          name="Email"
-          // value={name}
-          // onChange={handleChange}
-          icon="envelope"
-          iconPosition="left"
-        />
-        <Form.Input
-          required
-          label="Name"
-          placeholder="name"
-          name="name"
-          // value={name}
-          // onChange={handleChange}
-          icon="user"
-          iconPosition="left"
-        />
-        <Form.Input
-          required
-          label="Password"
-          placeholder="Password"
-          name="Password"
-          // value={name}
-          // onChange={handleChange}
-          icon="eye"
-          iconPosition="left"
-        />
+        <Form
+          onSubmit={handleSubmit}
+        >
+          <Form.Input
+            required
+            label="Email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            // value={name}
+            // onChange={handleChange}
+            icon="envelope"
+            iconPosition="left"
+          />
+          <Form.Input
+            required
+            label="Name"
+            placeholder="name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            // onChange={handleChange}
+            icon="user"
+            iconPosition="left"
+          />
+          <Form.Input
+            required
+            label="Password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            type="password"
+            // value={name}
+            // onChange={handleChange}
+            icon="eye"
+            iconPosition="left"
+          />
 
-        <Form.Dropdown placeholder='role' value = 'dog'/>
-        <Button color='orange' content='Sign Up!' ></Button>
-
-
+          <Form.Select placeholder='role' options={options} name='role' onChange={(e) => {
+            setUser((prev) => ({ ...prev, role: e.target.innerText }))
+    
+          }
+          } />
+          <Button color='orange' content='Sign Up!' type='submit' />
+        </Form>
       </Segment>
     </Layout>
   )
--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,13 +1,10 @@
+const { createUser } = require("../Controllers/user");
+
 const router = require("express").Router();
 
-const {
-  createUser,
-  getUsernameAvailable,
-  postUserLogin,
-} = require("../Controllers/user");
+
 
 router.route("/signup").post(createUser);
-router.route("/login").post(postUserLogin);
-router.route("/:username").get(getUsernameAvailable);
+
 
 module.exports = router;
