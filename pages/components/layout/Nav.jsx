29d3691e4473d353e6c96a6fb90c5b6aff451078
Daniel Chavez Ceniceros <Dchave263@west-mec.local>
2022-03-30 15:02:45 -0700

    Getting fixed homepage

--- pages/components/layout/Nav.jsx
+++ pages/components/layout/Nav.jsx
@@ -1,17 +1,42 @@
-import {ReactChild, useState} from 'react';
-import {Image} from "semantic-ui-react";
+import { ReactChild, useState } from "react";
+import { Image } from "semantic-ui-react";
 const Nav = () => {
   const [admin, setAdmin] = useState(false);
 
   return (
-    <div className="navbar" style={{position: "fixed", top: 0, width: "100%"}}>
-      <ul className="pageList" style={{listStyle: "none", display: "inline-flex"}}>
-        <li className="item"><a className="item" href="/index" style={{display: "inline-flex"}}><Image src="./DarkLogo.png" style={{width: "30px", height: "20px", paddingRight: "10px"}} className="logo"/> Home</a></li>
-        <li className="item"><a className="item" href="/adoption" style={{marginLeft: "15px"}}>For Adoption</a></li>
-        {admin && <li className="item"><a href="/admin" style={{marginLeft: "15px"}}>Admin</a></li>}
+    <div
+      className="navbar"
+      style={{ position: "fixed", top: 0, width: "100%" }}
+    >
+      <ul
+        className="pageList"
+        style={{ listStyle: "none", display: "inline-flex" }}
+      >
+        <li className="item">
+          <a className="item" href="/index" style={{ display: "inline-flex" }}>
+            <Image
+              src="./DarkLogo.png"
+              style={{ width: "30px", height: "20px", paddingRight: "10px" }}
+              className="logo"
+            />{" "}
+            Home
+          </a>
+        </li>
+        <li className="item">
+          <a className="item" href="/adoption" style={{ marginLeft: "15px" }}>
+            For Adoption
+          </a>
+        </li>
+        {admin && (
+          <li className="item">
+            <a href="/admin" style={{ marginLeft: "15px" }}>
+              Admin
+            </a>
+          </li>
+        )}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
