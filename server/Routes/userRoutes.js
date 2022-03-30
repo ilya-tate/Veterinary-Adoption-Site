8fef334e04c586705dee92aedca9fac4cc45bccc
Daniel Chavez Ceniceros <Dchave263@west-mec.local>
2022-03-30 14:26:03 -0700

    commiting successful login page

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,10 +1,12 @@
-const { createUser } = require("../Controllers/user");
-
 const router = require("express").Router();
 
+const {
+    createUser,
+    postUserLogin,
+} = require("../Controllers/user");
 
 
 router.route("/signup").post(createUser);
-
+router.route("/login").post(postUserLogin);
 
 module.exports = router;
