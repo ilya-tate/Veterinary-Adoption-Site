b65fbbcc93c27f1d2cae425fc87b566a85fb80c3
Ethan Philpott <Ephilp288@west-mec.local>
2022-04-19 18:24:48 -0700

    pretty much finished mobile indivual animal page

--- pages/animals/[aid].jsx
+++ pages/animals/[aid].jsx
@@ -12,6 +12,7 @@ import Right from "../../assets/svgs/right.svg"
 const dummyData = {
     name: "Ferdinaind",
     images: [gentleman, hotdog],
+    video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     age: .5,
     common: {
         desc: "Don't mess with the bull if you can't get the horns or something like that. This dog is mean, lean, and a fighting machine. Don't get on it's bad side or else you'll become... bad",
@@ -55,6 +56,71 @@ const IndividualAnimal = () => {
                 <h2 className={styles.name}>
                     {dummyData.name}
                 </h2>
+            </div>
+            <div className={styles.info}>
+                <div className={styles.video}>
+                    <iframe 
+                        width="100%"
+                        height="100%"
+                        className={styles.iframe}
+                        src={dummyData.video.replace("watch?v=", "embed/") + "?rel=0&showinfo=0&autohide=1&wmode=transparent"} 
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen
+                        wmode="transparent"
+                    ></iframe>
+                </div>
+                <div className={styles.common}>
+                    <h4 className={styles.commonInfo}>
+                        Common Information
+                    </h4>
+                    <ul className={styles.info}>
+                        <li className={styles.item  + " " + styles.noFlex}>
+                            <p className={styles.title}>Description:</p>
+                            <p className={styles.details}>{dummyData.common.desc}</p>
+                        </li>
+                        <li className={styles.item}>
+                            <p className={styles.title}>Breed:</p>
+                            <p className={styles.details}>{dummyData.common.breed}</p>
+                        </li>
+                        <li className={styles.item}>
+                            <p className={styles.title}>Sex:</p>
+                            <p className={styles.details}>{dummyData.common.sex}</p>
+                        </li>
+                        <li className={styles.item}>
+                            <p className={styles.title}>Neutered:</p>
+                            <p className={styles.details}>{dummyData.common.neutered ? "Yes" : "No"}</p>
+                        </li>
+                        <li className={styles.item}>
+                            <p className={styles.title}>Vaccinated:</p>
+                            <p className={styles.details}>{dummyData.common.vaccinated ? "Yes" : "No"}</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.specs}>
+                    <h4 className={styles.specInfo}>Specifications</h4>
+                    <div className={styles.info}>
+                        {dummyData.specs.needs.length ? <div className={styles.cata}>
+                            <h4 className={styles.title}>Needs:</h4>
+                            <ul className={styles.list}>
+                                {dummyData.specs.needs.map(need => <li>{need}</li>)}
+                            </ul>
+                        </div> : null}
+                        {dummyData.specs.cautions.length ? <div className={styles.cata}>
+                            <h4 className={styles.title}>Cautions:</h4>
+                            <ul className={styles.list}>
+                                {dummyData.specs.cautions.map(cauc => <li>{cauc}</li>)}
+                            </ul>
+                        </div> : null}
+                    </div>
+                </div>
+                <div className={styles.contact}>
+                    <button className={styles.button}>
+                        Contact a Teacher
+                    </button>
+                    <p>Make this animal a part of your family!</p>
+                </div>
             </div>
         </WithBoth>
     )
