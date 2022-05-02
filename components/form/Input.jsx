bc75dc00f4091ca6d7f8970d406c9a47ff7ad514
Ethan Philpott <ephilpott2004@gmail.com>
2022-05-02 14:56:30 -0700

    minor changes to input comp

--- components/form/Input.jsx
+++ components/form/Input.jsx
@@ -1,19 +1,22 @@
 import styles from "../../styles/components/form/Input.module.scss"
 
-const Input = ({name, type, icon, className, onChange}) => {
+const Input = ({name, type, icon, className, onChange, heading}) => {
     return (
-        <label htmlFor={name} className={styles.input + " " + className}>
-            <input
-                type={type || "text"}
-                name={name}
-                id={name}
-                className={styles.elem}
-                onChange={(e) => onChange(e.target.value)}
-            />
-            <div className={styles.icon}>
-                {icon}
-            </div>
-        </label>
+        <div className={styles.inputContainer}>
+            {heading && <div className={styles.heading}>{heading}</div>}
+            <label htmlFor={name} className={styles.input + " " + className}>
+                <input
+                    type={type || "text"}
+                    name={name}
+                    id={name}
+                    className={styles.elem}
+                    onChange={(e) => onChange(e.target.value)}
+                />
+                <div className={styles.icon}>
+                    {icon}
+                </div>
+            </label>
+        </div>
     )
 }
 
--- pages/admin/animals/[aid].jsx
+++ pages/admin/animals/[aid].jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import WithBoth from "../../../components/layout/with/WithBoth"
 import styles from "../../../styles/pages/admin/animals/[aid].module.scss"
+import Image from "../../../assets/svgs/image.svg"
 
 const EditAnimal = () => {
+    const [images, setImages] = useState([])
+    const [imageIndex, setImageIndex] = useState(0);
+
     return (
         <WithBoth className={styles.editAnimal}>
+            <div className={styles.images}>
+                {/* <div className={styles.main}>
+                    <div className={styles.slider}>
+                        {images.length ? <div className={styles.left} onClick={() => setImageIndex(i => i - 1 < 0 ? images.length - 1 : i - 1)}>
+                            <Left className={styles.icon} />
+                        </div> : null}
+                        <div className={styles.image}>
+                            <Image
+                                src={images[imageIndex] || <Image />}
+                                alt={dummyData.name}
+                                layout="fill"
+                                objectFit="cover"
+                                objectPosition="center"
+                                className={styles.elem}
+                            />
+                        </div>
+                        {images.length ? <div className={styles.right} onClick={() => setImageIndex(i => i + 1 > images.length - 1 ? 0 : i + 1)}>
+                            <Right className={styles.icon} />
+                        </div> : null}
+                    </div>
+                    <h2 className={styles.name}>
+                        {dummyData.name}
+                    </h2>
+                </div> */}
+                <button className={styles.uploadImage}>Upload Image</button>
+            </div>
+            <div className={styles.common}>
+                <h2>Common Information</h2>
+                <div className={styles.mainForm}>
 
+                </div>
+            </div>
         </WithBoth>
     )
 }
