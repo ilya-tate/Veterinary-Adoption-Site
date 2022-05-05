9563e2c6ea94086d3ec12dcc6cced6a5281f2751
Ethan Philpott <ephilpott2004@gmail.com>
2022-05-04 18:23:57 -0700

    worked on the add animal's page more and got the images working, finally

--- pages/admin/animals/[aid].jsx
+++ pages/admin/animals/[aid].jsx
@@ -4,7 +4,12 @@ import styles from "../../../styles/pages/admin/animals/[aid].module.scss"
 import Input from "../../../components/form/Input"
 import Area from '../../../components/form/Area'
 import Radios from '../../../components/form/Radios'
-import { useRouter } from 'next/router'
+import Image from "next/image"
+import TempImage from "../../../assets/svgs/image.svg"
+import Left from "../../../assets/svgs/left.svg"
+import Right from "../../../assets/svgs/right.svg"
+import X from "../../../assets/svgs/x.svg"
+import Video from "../../../assets/svgs/video.svg"
 
 const radios = {
     sex: [{name: "male", heading: "Male"}, {name: "female", heading: "Female"}],
@@ -14,34 +19,52 @@ const radios = {
 
 const EditAnimal = () => {
     const [images, setImages] = useState([])
+    const [imageIndex, setImageIndex] = useState(0);
+    const [video, setVideo] = useState(null)
+
+    const uploadImage = (e) => {
+        const files = [...e.target.files].map(file => ({file, name: file.name, url: URL.createObjectURL(file)}))
+        setImages((i) => [...i, ...files])
+    }
+
+    const deleteImage = (index) => {
+        if (imageIndex === images.length - 1 && images.length !== 1) setImageIndex(i => i - 1);
+        setImages((img) => img.filter((_, i) => i !== index));
+    }
 
     return (
         <WithBoth className={styles.editAnimal}>
             <div className={styles.images}>
-                {/* <div className={styles.main}>
+                <div className={styles.slide}>
                     <div className={styles.slider}>
-                        {images.length ? <div className={styles.left} onClick={() => setImageIndex(i => i - 1 < 0 ? images.length - 1 : i - 1)}>
+                        {images.length > 1 ? <div className={styles.left} onClick={() => setImageIndex(i => i - 1 < 0 ? images.length - 1 : i - 1)}>
                             <Left className={styles.icon} />
                         </div> : null}
-                        <div className={styles.image}>
-                            <Image
-                                src={images[imageIndex] || <Image />}
-                                alt={dummyData.name}
+                        <div className={styles.image + " " + (images[imageIndex] ? "" : styles.noImage)}>
+                            {images[imageIndex] ? <Image
+                                src={images[imageIndex].url}
+                                alt={images[imageIndex].name}
                                 layout="fill"
                                 objectFit="cover"
                                 objectPosition="center"
                                 className={styles.elem}
-                            />
+                            /> : <>
+                                <TempImage />
+                                <p>Upload an Image</p>
+                            </>}
                         </div>
-                        {images.length ? <div className={styles.right} onClick={() => setImageIndex(i => i + 1 > images.length - 1 ? 0 : i + 1)}>
+                        {images.length > 1 ? <div className={styles.right} onClick={() => setImageIndex(i => i + 1 > images.length - 1 ? 0 : i + 1)}>
                             <Right className={styles.icon} />
                         </div> : null}
+                        {images.length ? <div className={styles.del} onClick={() => deleteImage(imageIndex)}>
+                            <X className={styles.icon}/>
+                        </div> : null}
                     </div>
-                    <h2 className={styles.name}>
-                        {dummyData.name}
-                    </h2>
-                </div> */}
-                <button className={styles.uploadImage}>Upload Image</button>
+                </div>
+                <button className={styles.uploadImage}>
+                    <span className={styles.text}>Upload Image</span>
+                    <input className={styles.file} multiple accept="image/png,image/jpg,image/jpeg" type="file" name="images" id="images"  onChange={uploadImage}/>
+                </button>
             </div>
             <div className={styles.common}>
                 <h2 className={styles.heading}>Required</h2>
@@ -72,6 +95,25 @@ const EditAnimal = () => {
                             </div>
                         </li>
                     </ul>
+                </div>
+            </div>
+            <div className={styles.optional}>
+                <h2 className={styles.heading}>Optional</h2>
+                <div className={styles.video}>
+                    {video ? <iframe
+                        width="100%"
+                        height="100%"
+                        className={styles.iframe}
+                        src={video}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        wmode="transparent"
+                    ></iframe> : <>
+                        <Video />
+                        <p>Upload a Video</p>
+                    </>}
                 </div>
             </div>
         </WithBoth>
