d93d04917af1dbe8893017ef31a57d38622dc6ee
Not Default Profile <gamingc340@gmail.com>
2022-05-16 15:28:28 -0700

    animal controllers

--- server/Controllers/Animals.js
+++ server/Controllers/Animals.js
@@ -1,15 +1,38 @@
 const AnimalModel = require("../Models/AnimalModel");
 
-const getAllAnimals = async (req, res) => {
+const oneAni = async (req, res) => {
+  const { params } = req.body;
   try {
-    console.log("hi");
-    let animals = await AnimalModel.find({})
-      .sort({ createdAt: -1 })
-      console.log("bye");
+    const theOne = await AnimalModel.findOne({ params });
+    return res.status(200).json(theOne);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllAnimals = async (req, res) => {
+  const { page } = req.query;
+
+  const pageNumber = Number(page);
+  const size = 8;
+  let animals;
+
+  try {
+    if (pageNumber === 1) {
+      animals = await AnimalModel.find()
+        .limit(size)
+        .sort({ createdAt: -1 })
+    } else {
+      const skips = size * (pageNumber - 1);
+      animals = await AnimalModel.find()
+        .skip(skips)
+        .limit(size)
+        .sort({ createdAt: -1 })
+    }
     return res.status(200).json(animals);
   } catch (error) {
     console.log(error);
-    return res.status(500).send("Server Error @ getDisplayAnimals");
+    return res.status(500).send("Server Error @ getAllAnimals in Controllers");
   }
 };
 
@@ -18,9 +41,9 @@ const getDisplayAnimals = async (req, res) => {
 
   try {
     let animals = await AnimalModel.find()
-      .limit(3)
+      .limit(size)
       .sort({ createdAt: -1 })
-      .populate("animal");
+      console.log("ANIMALS ANIMALS ANIMALS", animals);
     return res.status(200).json(animals);
   } catch (error) {
     console.log(error);
@@ -30,8 +53,9 @@ const getDisplayAnimals = async (req, res) => {
 
 const getAnimalById = async (req, res) => {
   try {
-    const animal = await AnimalModel.findById(req.params.postId)
-      .populate("animals")
+    const animal = await AnimalModel.findById(req.params.postId).populate(
+      "animals"
+    );
     if (!animal) return res.status(403).send("Animal Not Found");
 
     return res.status(200).json(animal);
@@ -41,4 +65,4 @@ const getAnimalById = async (req, res) => {
   }
 };
 
-module.exports = {getDisplayAnimals, getAllAnimals};
+module.exports = { getDisplayAnimals, getAllAnimals, oneAni };
