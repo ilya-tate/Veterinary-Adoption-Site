2c17b9d0fc39d01218e907c8e0488e79138e011b
Not Default Profile <gamingc340@gmail.com>
2022-05-16 15:21:20 -0700

    same but event

--- pages/adminEvent.jsx
+++ pages/adminEvent.jsx
@@ -1,179 +1,174 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+import { Icon } from "semantic-ui-react";
 
 const event = () => {
   const [submitted, setSubmitted] = useState(false);
   const [clicked, setClicked] = useState(false);
   const [newEvent, setNewEvent] = useState({
     title: "",
-    time: Date.now(),
-    date: Date.now(),
+    time: "",
+    date: "",
     description: "",
     location: "",
     purpose: "",
   });
-  // const [name, setName] = useState("")
-  // const [name, setName] = useState("")
-  // const [name, setName] = useState("")
+  const [show, setShow] = useState(false);
+  const [sucessful, setSucessful] = useState(false);
+  const [trash, setTrash] = useState(false);
+  const [del, setDel] = useState({});
+  const [delId, setDelId] = useState("");
+  const [events, setEvents] = useState([]);
+  const [newDate, setNewDate] = useState(new Date());
+  const [password, setPassword] = useState(99550);
+  const [clearAll, setClearAll] = useState(false);
 
-  const changeSubmit = () => {
-    setTimeout(() => {
-      setSubmitted(true);
-      setClicked(false)
-    }, 1500);
+  const getInfo = async () => {
+    const { data } = await axios.get("/api/v1/events");
+    setEvents(data);
   };
+
+  const clear = async () => {
+    const { data } = await axios.delete("/api/v1/events/");
+    console.log(data);
+    setEvents(data);
+    getInfo();
+  };
+
+  useEffect(() => {
+    getInfo();
+    console.log(events);
+  }, []);
+
+  useEffect(() => {
+    getInfo();
+    setSucessful(false);
+  }, [sucessful]);
 
   const handleChange = (e) => {
     const { name, files, value } = e.target;
     if (name === "media") {
       setMedia(files[0]);
       setMediaPreview(URL.createObjectURL(files[0]));
-    } else if (name === "spade") {
-      setSpade(!spade);
-      console.log(spade);
     }
+    // else if (name == "spayed") {
+    //   setSpade(!spade);
+    //   console.log(spade);
+    //   setNewevent((prev) => ({ ...prev, [spayed]: spade }));
+    // }
     setNewEvent((prev) => ({ ...prev, [name]: value }));
+    console.log(newEvent.name);
+  };
+
+  const changeSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+      setClicked(false);
+    }, 1500);
+    setTimeout(() => {
+      setSubmitted(false);
+    }, 3000);
+    setSucessful(true);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    let picUrl;
+    // setLoading(true);
+    // let picUrl;
 
-    if (media) {
-      const formData = new FormData();
-      formData.append("image", media, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-      const res = await axios.post("/api/v1/events/admin", formData);
-      picUrl = res.data.src;
-    }
+    // if (media) {
+    // const formData = new FormData();
+    // formData.append("image", media, {
+    //   headers: {
+    //     "Content-Type": "multipart/form-data",
+    //   },
+    // });
+    console.log(newEvent);
+    const res = await axios.post("/api/v1/events/admin", {
+      ...newEvent,
+      createdAt: Date.now(),
+    });
+    console.log(res);
+    setSucessful(true);
+    // }
+  };
 
-    await submitNewevent(
-      newevent.name,
-      newevent.age,
-      newevent.gender,
-      newevent.event,
-      newevent.breed,
-      newevent.colors,
-      newevent.needs,
-      newevent.details,
-      newevent.description,
-      newevent.vaccines,
-      newevent.spade,
-      picUrl,
-      newevent,
-      setNewevent,
-      setError
-    );
+  const chooseDrop = async (e) => {
+    console.log("DROP");
+    // const {data} = await axios.get("/api/v1/events/oneAni", e.target.value)
+    setDel(events.find((event) => e.target.value.toString() == event.title));
+    console.log(del.date);
+    setNewDate(del.date);
+    console.log(newDate, "newDate");
+  };
 
-    console.log("submitted");
-
-    setMedia(null);
-    setMediaPreview(null);
-    setLoading(false);
+  const deleteEvent = async () => {
+    await axios.delete(`/api/v1/events/admin/${delId}`);
+    setSucessful(true);
+    setShow(false);
   };
 
   return (
     <div className="eventMain">
-      <div className="createEvent" onSubmit={handleSubmit}>
+      <form className="createEvent" onSubmit={handleSubmit}>
         <input
           type="text"
-          name="name"
-          placeholder="Name..."
+          name="title"
+          placeholder="Event Name..."
           className="eventName eventInputs"
           onChange={handleChange}
-          value={newEvent.name}
+          value={newEvent.title}
+        />
+        <input
+          type="date"
+          name="date"
+          className="eventDate eventInputs"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+          value={newEvent.date}
+        />
+        <input
+          type="time"
+          name="time"
+          className="eventTime eventInputs"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+          value={newEvent.time}
         />
         <input
           type="text"
-          name="gender"
-          placeholder="Gender..."
-          className="eventGender eventInputs"
+          name="location"
+          placeholder="Event Location..."
+          className="eventInputs"
           onChange={handleChange}
-          value={newEvent.gender}
-        />
-        <input
-          type="number"
-          name="age"
-          placeholder="Age..."
-          className="age eventInputs"
-          onChange={handleChange}
-          value={newEvent.age}
-        />
+          value={newEvent.location}
+        ></input>
         <input
           type="text"
-          name="event"
-          placeholder="event Type..."
-          className="eventType eventInputs"
+          name="purpose"
+          placeholder="Purpose Of Event..."
+          className="eventInputs"
           onChange={handleChange}
-          value={newEvent.event}
+          value={newEvent.purpose}
         />
-        <input
-          type="text"
-          name="breed"
-          placeholder="Breed..."
-          className="eventBreed eventInputs"
-          onChange={handleChange}
-          value={newEvent.breed}
-        />
-        <input
-          type="text"
-          name="colors"
-          placeholder="Colors..."
-          className="eventColors eventInputs"
-          onChange={handleChange}
-          value={newEvent.colors}
-        />
-        <input
-          type="text"
-          name="needs"
-          placeholder="Needs..."
-          className="eventNeeds eventInputs"
-          onChange={handleChange}
-          value={newEvent.needs}
-        />
-        <input
-          type="text"
-          name="details"
-          placeholder="Details..."
-          className="eventDetails eventInputs"
-          onChange={handleChange}
-          value={newEvent.details}
-        />
-        <input
+        <textarea
+          rows="5"
           type="text"
           name="description"
-          placeholder="Description..."
-          className="eventDesc eventInputs"
+          placeholder="Event Description..."
+          className="eventInputs"
           onChange={handleChange}
           value={newEvent.description}
         />
-        <input
-          type="text"
-          name="Vaccination"
-          placeholder="Vaccines..."
-          className="eventVaccines eventInputs"
-          onChange={handleChange}
-          value={newEvent.vaccinations}
-        />
-        <div className="spadeDiv">
-          <p className="spade">Spade?</p>
-          <input
-            type="checkbox"
-            name="Spade"
-            className="eventSpade eventInputs"
-            onChange={handleChange}
-            value={newEvent.spade}
-          />
-        </div>
         <br />
         <button
           type="submit"
           onClick={() => {
             setClicked(true);
             changeSubmit();
+            getInfo();
           }}
           className={
             clicked
@@ -183,8 +178,136 @@ const event = () => {
               : "submit"
           }
         >
-          {clicked ? "" : submitted ? "✔" : "Submit?"}
+          {clicked ? "loading" : submitted ? "✔" : "Submit?"}
         </button>
+        <p className="help">
+          What To Put For Location
+          <a href="https://docs.google.com/presentation/d/1RgojzIgqlQjeovyOV87aEkhmxRbwLfi2Uv0IeKbXyIY/edit#slide=id.g1272d95724a_0_5">
+            <Icon className="helpIcon" name="help circle" />
+          </a>
+        </p>
+      </form>
+
+      {/*
+            Remove event and Create event barrier so that I can see the difference
+          */}
+
+      <div className="removeEvent">
+        <label htmlFor="events">
+          Events:
+          <select
+            name="events"
+            onChange={(e) => {
+              chooseDrop(e);
+              setDelId(
+                e.target.children[e.target.selectedIndex].getAttribute("id")
+              );
+              setShow(false);
+              console.log("hi");
+            }}
+          >
+            <option value=""></option>
+            {events.map((event) => {
+              return (
+                <option id={event._id} value={event.title}>
+                  {event.title}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+        <br />
+        <br />
+        {!show && (
+          <button
+            onClick={() => {
+              getInfo();
+              setShow(true);
+              {
+                setTimeout(() => {
+                  console.log("DEL", del);
+                }, 2000);
+              }
+            }}
+            className="findAni"
+          >
+            Find event
+          </button>
+        )}
+        {show && del !== "" && (
+          <div className="delDiv">
+            {del.location ? (
+              <iframe
+                src={del.location}
+                allowFullScreen=""
+                loading="fast"
+                className="map"
+              ></iframe>
+            ) : (
+              <iframe className="map"></iframe>
+            )}
+            <div className="delBot">
+              {del.title ? (
+                <h1 className="delName">
+                  {console.log(newDate)}
+                  {del.title} <br />
+                  {
+                    // newDate.getMonth().toString().padStart(2, "0") +
+                    //   "/" +
+                    //   newDate.getDate().toString().padStart(2, "0") +
+                    //   "/" +
+                    //   newDate.getFullYear()
+                    del.date && del.date.split("T")[0]
+                  }
+                  {del.time}
+                </h1>
+              ) : (
+                <h1 className="delName"></h1>
+              )}
+              <button
+                className="delete"
+                onMouseEnter={() => setTrash(true)}
+                onMouseLeave={() => setTrash(false)}
+                onClick={() => {
+                  deleteEvent();
+                  getInfo();
+                }}
+              >
+                <Icon
+                  name={trash ? "trash alternate outline" : "trash"}
+                  color="red"
+                  onClick={() => setShow(false)}
+                />
+              </button>
+            </div>
+          </div>
+        )}
+        <br />
+        <a
+          className="clear"
+          onClick={() => {
+            setClearAll(true);
+            console.log("CLEAR...SUCCESSFUL");
+          }}
+        >
+          {!clearAll && <p>Clear All?</p>}
+          {clearAll && (
+            <input
+              type="number"
+              placeholder="Enter Password"
+              onBlur={(e) => {
+                password === e.target.value
+                  ? () => {
+                      clear();
+                      setTimeout(() => {
+                        window.location.replace("/events");
+                      }, 100);
+                    }
+                  : clearAll(false);
+              }}
+            ></input>
+          )}
+        </a>
       </div>
     </div>
   );
