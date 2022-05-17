7335555f3e8f46ccce7adedd9b4dab8d9e5d71cb
Not Default Profile <gamingc340@gmail.com>
2022-05-17 13:13:00 -0700

    maybe animals?

--- server/Routes/animals.js
+++ server/Routes/animals.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 
-const {getAllAnimals, getDisplayAnimals} = require("../Controllers/Animals")
-const {addAnimal, removeAnimal} = require("../Controllers/Admin");
+const {getDisplayAnimals, getAllAnimals, oneAni} = require("../Controllers/Animals")
+const {addAnimal, removeAnimal, clearANI} = require("../Controllers/Admin");
+const {uploadAnimalPic} = require("../Controllers/uploadAnimalPics");
 
 router.route("/adoption").get(getAllAnimals)
-router.route("/admin").post(addAnimal).delete(removeAnimal)
-router.route("/").get(getDisplayAnimals);
+router.route("/oneAni").get(oneAni);
+router.route("/admin").post(addAnimal)
+router.route('/admin/:id').delete(removeAnimal)
+router.route("/").get(getDisplayAnimals).delete(clearANI);
+router.route("/upload").post(uploadAnimalPic);
 
 module.exports = router;
