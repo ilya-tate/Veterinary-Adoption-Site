96c292e0c1d2e866f8e7c2ea5d5b6f462b1378d3
Not Default Profile <gamingc340@gmail.com>
2022-05-17 13:14:25 -0700

    fixing pic upload

--- server/Controllers/uploadPic.js
+++ server/Controllers/uploadPic.js
@@ -3,14 +3,19 @@ const fs = require("fs");
 
 const uploadAnimalPic = async (req, res) => {
   try {
+    // console.log("stuff");
     const src = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
       use_filename: true,
-      folder: "ANIMAL PICS",
+      folder: "Social Session 1",
     });
+    // console.log("stuff");
     fs.unlinkSync(req.files.image.tempFilePath);
+    console.log(src);
+    console.log(req.files.image.tempFilePath);
     return res.status(200).json({ src: src.secure_url });
   } catch (error) {
     console.log(error);
+    console.error("CLOUDINARY ERROR")
     return res.status(500).send("Cloundinary Upload Error");
   }
 };
