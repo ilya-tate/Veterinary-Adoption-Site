84ad79e1be4a87d08e266c9c9105b1a881bfa4a6
John McFarland <Jmcfar187@west-mec.local>
2022-05-23 14:56:40 -0700

    lsdew

--- server/Routes/authRoute.js
+++ server/Routes/authRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { getUserAuth } = require("../controllers/auth");
+const { getUserAuth } = require("../Controllers/auth");
 const { auth } = require("../middleware/auth");
 
 router.route("/").get(auth, getUserAuth);
